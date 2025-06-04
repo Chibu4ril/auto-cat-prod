b65c6a237354fc27b741723e595028bc15e3da7a
Innocent <successskido@gmail.com>
2025-06-04 17:59:31 +0100

    update

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
       <h1 className="font-bold lg:text-7xl text-center leading-relaxed">
-        Your Product Catalog Automation System
+        Your Product Catalog <br /> Automation System
       </h1>
       <p>Powered By</p>
       <div className="flex gap-8 justify-center items-center">
